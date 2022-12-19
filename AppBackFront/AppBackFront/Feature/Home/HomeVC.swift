005c2b1ff9823bf489460e71491e1d60409bfe9f
Caio Fabrini <caiofabrini@outlook.com>
2022-12-19 09:11:05 -0300

    criando TabBar

---
+++ AppBackFront/AppBackFront/Feature/Home/HomeVC.swift
@@ -0,0 +1,19 @@
+//
+//  HomeVC.swift
+//  AppBackFront
+//
+//  Created by Caio Fabrini on 19/12/22.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+    }
+
+
+}
