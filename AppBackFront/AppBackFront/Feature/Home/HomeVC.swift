1512fa55ffb753288e077f47af530b3becb05d82
Caio Fabrini <caiofabrini@outlook.com>
2022-12-20 16:01:36 -0300

    exibindo celula de NFT na Home

--- AppBackFront/AppBackFront/Feature/Home/HomeVC.swift
+++ AppBackFront/AppBackFront/Feature/Home/HomeVC.swift
@@ -33,7 +33,11 @@ class HomeVC: UIViewController {
 extension HomeVC: HomeViewModelDelegate {
     func success() {
         print(#function)
-        screen?.configCollectionViewProtocols(delegate: self, dataSource: self)
+        DispatchQueue.main.async {
+            self.screen?.configCollectionViewProtocols(delegate: self, dataSource: self)
+            self.screen?.configTableViewProtocols(delegate: self, dataSource: self)
+            self.screen?.tableView.reloadData()
+        }
     }
     
     func error() {
@@ -58,4 +62,21 @@ extension HomeVC: UICollectionViewDelegate, UICollectionViewDataSource, UICollec
         return viewModel.sizeForItemAt
     }
 
+}
+
+extension HomeVC: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.numberOfRowsInSection
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: NftTableViewCell.identifier, for: indexPath) as? NftTableViewCell
+        cell?.setupCell(data: viewModel.loadCurrentNft(indexPath: indexPath))
+        return cell ?? UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return viewModel.heightForRowAt
+    }
 }
