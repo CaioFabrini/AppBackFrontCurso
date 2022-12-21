707e1c248fdb8bd747762334c8dcab1b89065332
Caio Fabrini <caiofabrini@outlook.com>
2022-12-21 09:44:02 -0300

    criacao da tela de detalhe

---
+++ AppBackFront/AppBackFront/Feature/NftDetail/ViewModel/NftDetailViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  NftDetailViewModel.swift
+//  AppBackFront
+//
+//  Created by Caio Fabrini on 21/12/22.
+//
+
+import UIKit
+
+class NftDetailViewModel {
+
+    let nft: Nft
+    
+    init(nft: Nft) {
+        self.nft = nft
+    }
+    
+    
+    
+    
+}
